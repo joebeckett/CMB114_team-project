b1e8694414a89839b9eb91ac809aef0cfceb52e8
canerkarakoc <canerkarakoc973@gmail.com>
2026-04-27 13:23:46 +0100

    Theoretical Half Life

--- Caner/Chemical Reaction Kinetics/reaction_caner.py
+++ Caner/Chemical Reaction Kinetics/reaction_caner.py
@@ -58,3 +58,27 @@ class Reaction:
             return "s^-1"
 
         return "dm^3 mol^-1 s^-1"
+    
+    
+    def get_theoretical_half_life(self):
+        """
+        Calculates theoretical half-life.
+
+        First order: t1/2 = ln(2) / k
+        Second order: t1/2 = 1 / k[A]0
+        """
+
+        if self.order == 1:
+            return math.log(2) / self.rate_constant
+
+        return 1 / (self.rate_constant * self.initial_concentration)
+
+    def get_description(self):
+        return (
+            f"Reaction name: {self.name}\n"
+            f"Reaction order: {self.order}\n"
+            f"Rate equation: {self.get_rate_equation()}\n"
+            f"Rate constant: {self.rate_constant} {self.get_units_for_k()}\n"
+            f"Initial concentration: {self.initial_concentration} mol dm^-3"
+        )
+
